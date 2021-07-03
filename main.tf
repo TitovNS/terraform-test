5c96d547e4c24d73c695601401e5257f9d3d2eec
Titov Nikita <titov.nikita.work@gmail.com>
2021-07-03 14:35:18 +0700

    add web-server deployment

--- main.tf
+++ main.tf
@@ -2,11 +2,30 @@ provider "aws" {
   region = "us-east-2"
 }
 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "my_Ubuntu" {
-  ami           = "ami-00399ec92321828f5"
-  instance_type = "t2.micro"
+  ami                    = "ami-00399ec92321828f5"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
     Name = "terraform-test"
   }
+
 }
