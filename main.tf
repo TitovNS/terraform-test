86791645f601770ee9bd2eefb64516c3366a4b40
Titov Nikita <titov.nikita.work@gmail.com>
2021-07-03 17:57:42 +0700

    added deployment of web-server cluster and balancer

--- main.tf
+++ main.tf
@@ -4,8 +4,12 @@ provider "aws" {
 
 variable "server_PORT" {
   description = "The port the server will use for HTTP requests"
-  type        = "number"
+  type        = number
   default     = "8080"
+}
+variable "alb_port" {
+  type    = number
+  default = "80"
 }
 
 resource "aws_security_group" "instance" {
@@ -19,19 +23,122 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_instance" "my_Ubuntu" {
-  ami                    = "ami-00399ec92321828f5"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_security_group" "alb" {
+  name = "terraform-exaple-alb"
+
+  ingress {
+    from_port   = var.alb_port
+    to_port     = var.alb_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_launch_configuration" "my_Ubuntu" {
+  image_id        = "ami-00399ec92321828f5"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_PORT} &
               EOF
 
-  tags = {
-    Name = "terraform-test"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "my_Ubuntu" {
+  launch_configuration = aws_launch_configuration.my_Ubuntu.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "my_Ubuntu" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.my_Ubuntu.arn
+  port              = var.alb_port
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_PORT
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
   }
 
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.my_Ubuntu.dns_name
+  description = "The domain name of the load balancer"
 }
